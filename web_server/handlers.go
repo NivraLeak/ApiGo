50ff24f86d5ca20a0fe4566a5f6ec56d3d096fda
nivra <nivraleak@outlook.com>
2020-11-22 00:19:52 -0500

    Agregando ruta api test

--- web_server/handlers.go
+++ web_server/handlers.go
@@ -8,3 +8,7 @@ import (
 func HandlerRoot(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello world")
 }
+
+func HandleHome(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "This is a api endpoint test")
+}
--- web_server/main.go
+++ web_server/main.go
@@ -3,5 +3,7 @@ package main
 func main() {
 	server := NewServer(":3001")
 	server.Handle("/", HandlerRoot)
+	server.Handle("/api", HandleHome)
+
 	server.Listen()
 }
